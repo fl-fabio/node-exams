3657b501dd9d5116c5cb5d22d677a5e02ccebaa5
fl-fabio <fl.fabio.lanzafame@gmail.com>
2023-02-22 12:35:42 +0100

    feature: insert functions in app

--- build/app.js
+++ build/app.js
@@ -4,4 +4,19 @@ const posts_1 = require("./data/posts");
 const searchById = () => {
     return posts_1.posts.filter(({ id }) => id >= 25 && id <= 41);
 };
-console.log(searchById());
+const getBodyLength = () => {
+    let lengthOfBody;
+    lengthOfBody = posts_1.posts.map((item) => {
+        return Object.assign(Object.assign({}, item), { len: item.body.length });
+    });
+    return lengthOfBody;
+};
+const maxBodyLength = () => {
+    let length;
+    length = posts_1.posts.map(({ body }) => body.length);
+    return Math.max(...length);
+};
+const findPostByBodyLength = () => {
+    return posts_1.posts.find(({ body }) => body.length === maxBodyLength());
+};
+console.log((findPostByBodyLength()));
--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,24 @@ const searchById = () : Post[] => {
     return posts.filter(({id}) => id >= 25 && id <= 41 )
 }
 
-const searchById2 = () : Post[] => {
-    return posts.filter(({id}) => id >= 25 && id <= 41 )
+const getBodyLength = () : object[] => {
+    let lengthOfBody;
+    lengthOfBody = posts.map((item) => {
+        return {...item, len: item.body.length}
+    } )
+    return lengthOfBody;
+}
+
+const maxBodyLength = () : number => {
+    let length: number[];
+    length = posts.map(({body}) => body.length)
+    return Math.max(...length);
+}
+
+const findPostByBodyLength = () => {
+    return posts.find(({body}) => body.length === maxBodyLength() );
 }
 
 
 
-console.log(searchById())
+console.log((findPostByBodyLength()));
